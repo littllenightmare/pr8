5c0e5ed91704cca5110f96b5217c09376097a2f0
исп-31 <-31@LAB17-13>
2023-11-03 13:42:09 +0300

    Добавьте файлы проекта.

---
+++ pr8/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr8
+{
+    class worker : man/*,*//* ICloneable, IComparable*/
+    {
+        string Name, Surname, Sex;
+
+        //конструктор
+        public string sex()
+        {
+            if (Sex == "M")
+            {
+                Sex = "пол мужской";
+                return Sex;
+            }
+            else
+            {
+                Sex = "пол женский";
+                return Sex;
+            }
+        }
+     }
+    class worker_dad:worker, man
+    { 
+     int Children;
+    }
+}
---
+++ pr8/Interface1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr8
+{
+    interface man
+    {
+        string name() { return name(); }
+        string surname() { return surname(); }
+        string sex() { return sex(); }
+        int children() { return children(); }
+    }
+}
